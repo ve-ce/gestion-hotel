b0baf6c4d9c5a689c4d6673698aa640d52f1867d
ve-ce <vencelest@gmail.com>
2023-04-16 19:49:36 -0400

    projet-cs

---
+++ GestionClientsHotels/Client.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionClientsHotels
+{
+    internal class Client
+    {
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public string Courriel { get; set; }
+        public string CarteCredit { get; set; }
+        public int NumeroChambre { get; set; }
+
+        public Client(string nom, string prenom, string courriel, string carteCredit, int numeroChambre)
+        {
+            Nom = nom;
+            Prenom = prenom;
+            Courriel = courriel;
+            CarteCredit = carteCredit;
+            NumeroChambre = numeroChambre;
+        }
+    }
+}
